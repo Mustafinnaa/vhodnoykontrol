50456e58a54b30974dbf005cac022a0eac5e0dae
Asus <Asus@DESKTOP-F2RLVQ4>
2024-09-03 16:48:07 +0300

    Добавьте файлы проекта.

---
+++ vhodnoykontrol/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vhodnoykontrol
+{
+    static class Data
+    {
+        public static List<Info> Info = new List<Info>
+        {
+            new Info(10153, "Чупашева А.И.", "01.10.2015"),
+            new Info(20174, "Иванов А.В.", "10.01.2017"),
+            new Info(30191, "Кривцов О.П.", "05.02.2019"),
+            new Info(40140, "Янаева Э.С.", "15.12.2014")
+        };
+    }
+
+    internal class Info
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public string Date { get; set; }
+
+        public Info(int id, string fio, string date)
+        {
+            Id = id;
+            FIO = fio;
+            Date = date;
+        }
+    }
+}
---
+++ vhodnoykontrol/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace vhodnoykontrol
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<CommissionResult> Results { get; set; } = new ObservableCollection<CommissionResult>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComboBox();
+            ResultsListView.ItemsSource = Results;
+        }
+
+        private void InitializeComboBox()
+        {
+            FIOComboBox.ItemsSource = Data.Info.Select(i => i.FIO).ToList();
+        }
+
+        private void FIOComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedFIO = FIOComboBox.SelectedItem as string;
+            if (selectedFIO != null)
+            {
+                var info = Data.Info.FirstOrDefault(i => i.FIO == selectedFIO);
+                if (info != null)
+                {
+                    DateTextBox.Text = info.Date;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedFIO = FIOComboBox.SelectedItem as string;
+            if (selectedFIO != null)
+            {
+                var info = Data.Info.FirstOrDefault(i => i.FIO == selectedFIO);
+                if (info != null)
+                {
+                    if (string.IsNullOrWhiteSpace(Vyruchka.Text) || string.IsNullOrWhiteSpace(DateTextBox.Text))
+                    {
+                        MessageBox.Show("заполните все поля!");
+                        return;
+                    }
+
+                    if (!decimal.TryParse(Vyruchka.Text, out decimal vyruchka))
+                    {
+                        MessageBox.Show("введите корректную сумму выручки");
+                        return;
+                    }
+
+                    if (!DateTime.TryParseExact(DateTextBox.Text, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime Date))
+                    {
+                        MessageBox.Show("введите корректную дату в формате дд.мм.гггг.");
+                        return;
+                    }
+
+                    if (info.Date != DateTextBox.Text)
+                    {
+                        MessageBox.Show("дата приема на работу не может быть изменена");
+                        return;
+                    }
+
+                    int yearsOfService = (DateTime.Now - Date).Days / 365;
+                    decimal commission = CalculateCommission(vyruchka, yearsOfService);
+
+                    var result = Results.FirstOrDefault(r => r.FIO == info.FIO);
+                    if (result != null)
+                    {
+                        result.Commission = commission;
+                        result.Vyruchka = vyruchka;
+                        result.Years = yearsOfService;
+                    }
+                    else
+                    {
+                        Results.Add(new CommissionResult
+                        {
+                            FIO = info.FIO,
+                            Commission = commission,
+                            Vyruchka = vyruchka,
+                            Years = yearsOfService
+                        });
+                    }
+                    FIOComboBox.SelectedIndex = -1;
+                    DateTextBox.Text = string.Empty;
+                    Vyruchka.Text = string.Empty;
+                }
+            }
+        }
+
+        private decimal CalculateCommission(decimal vyruchka, int yearsOfService)
+        {
+            decimal commissionRate = vyruchka < 50000 ? 0.03m : 0.06m;
+            if (yearsOfService > 3)
+            {
+                commissionRate += 0.05m;
+            }
+            return vyruchka * commissionRate;
+        }
+
+     
+    }
+
+    public class CommissionResult
+    {
+        public string FIO { get; set; }
+        public decimal Commission { get; set; }
+        public decimal Vyruchka { get; set; }
+        public int Years { get; set; }
+    }
+}
